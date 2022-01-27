481bf6660778c89dffdbd8853e6f209bd4542e50
AimanK <Akaya005@fiu.edu>
2022-01-26 21:04:02 -0500

    Typo fix

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ApplicationRecord
     validates :title, presence: true
-    validates :author, prescence: true
+    validates :author, presence: true
     validates :body, presence: true, length: { minimum: 10 }
 end
